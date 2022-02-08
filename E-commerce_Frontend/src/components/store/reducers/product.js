97c50a8364d8ad133a12519c86d8602bcc1512a1
Yuri Ferreira <64503593+Yuri-Ferreira-27@users.noreply.github.com>
2022-02-08 14:00:47 -0300

    Versão Final
    Essa versão foi clonada e alterada do repositório passado no link do bootcamp Inter Front-End Developer

---
+++ E-commerce_Frontend/src/components/store/reducers/product.js
@@ -0,0 +1,180 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Fluminense 2020",
+      price: 300,
+      image: "./images/produtos/flu20.jpg",
+      id_categorys: 1,
+      name_categorys: "Clubes brasileiros",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Palmeiras 2020",
+      price: 350,
+      image: "./images/produtos/palmeiras.jpg",
+      id_categorys: 1,
+      name_categorys: "Clubes brasileiros",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Grêmio 2020",
+      price: 230,
+      image: "./images/produtos/gremio.jpg",
+      id_categorys: 1,
+      name_categorys: "Clubes brasileiros",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "europeu",
+      fk_idcategorys: 2,
+      name_product: "Arsenal 2020/21",
+      price: 399.99,
+      image: "./images/produtos/arsenal.jpg",
+      id_categorys: 2,
+      name_categorys: "Clubes europeus",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "europeu",
+      fk_idcategorys: 2,
+      name_product: "Manchester City 2020/21",
+      price: 350.99,
+      image: "./images/produtos/man-city.jpg",
+      id_categorys: 2,
+      name_categorys: "Clubes europeus",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "europeu",
+      fk_idcategorys: 2,
+      name_product: "Milan 2020/21",
+      price: 299.99,
+      image: "./images/produtos/milan.jpg",
+      id_categorys: 2,
+      name_categorys: "Clubes europeus",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "historica",
+      fk_idcategorys: 3,
+      name_product: "Brasil 1994",
+      price: 200,
+      image: "./images/produtos/brasil94.jpg",
+      id_categorys: 3,
+      name_categorys: "Camisas históricas",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "historica",
+      fk_idcategorys: 3,
+      name_product: "Fluminense 1985",
+      price: 450,
+      image: "./images/produtos/flu85.jpg",
+      id_categorys: 3,
+      name_categorys: "Camisas históricas",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "selecao",
+      fk_idcategorys: 4,
+      name_product: "Alemanha 2020",
+      price: 350,
+      image: "./images/produtos/alemanha.jpg",
+      id_categorys: 4,
+      name_categorys: "Seleções nacionais",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "selecao",
+      fk_idcategorys: 4,
+      name_product: "Brasil 2020",
+      price: 399,
+      image: "./images/produtos/brasil.jpg",
+      id_categorys: 4,
+      name_categorys: "Seleções nacionais",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "nacional",
+      fk_idcategorys: 5,
+      name_product: "Fluminense 2021",
+      price: 300,
+      image: "./images/produtos/flu21.jpg",
+      id_categorys: 5,
+      name_categorys: "Clubes brasileiros",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "casual",
+      fk_idcategorys: 5,
+      name_product: "Real Madrid 2019/20",
+      price: 189.99,
+      image: "./images/produtos/real_treino.jpg",
+      id_categorys: 5,
+      name_categorys: "Casuais",
+      selected: false
+    },
+    {
+      id_product: 13,
+      category: "nacional",
+      fk_idcategorys: 5,
+      name_product: "Fluminense 2020 Verde",
+      price: 300,
+      image: "./images/produtos/flu20-verde.jpg",
+      id_categorys: 5,
+      name_categorys: "Clubes brasileiros",
+      selected: false
+    },
+    {
+      id_product: 14,
+      category: "nacional",
+      fk_idcategorys: 5,
+      name_product: "Fluminense 2020 Branca",
+      price: 300,
+      image: "./images/produtos/flu20-branca.jpg",
+      id_categorys: 5,
+      name_categorys: "Clubes brasileiros",
+      selected: false
+    },
+    {
+    id_product: 15,
+      category: "europeu",
+      fk_idcategorys: 2,
+      name_product: "Manchester United 2020/21",
+      price: 350.99,
+      image: "./images/produtos/man-united.jpg",
+      id_categorys: 2,
+      name_categorys: "Clubes europeus",
+      selected: false
+    },
+    {
+      id_product: 16,
+        category: "europeu",
+        fk_idcategorys: 2,
+        name_product: "Real Madrid 2020/21",
+        price: 350.99,
+        image: "./images/produtos/real_jogo.jpg",
+        id_categorys: 2,
+        name_categorys: "Clubes europeus",
+        selected: false
+      },
+];
+
+export default productsList;
